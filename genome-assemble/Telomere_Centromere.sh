a68c15b1b4fd4d3fa11a26e6ca89440c90410ebb
Zhang-Shufeng <148443531+Zhang-Shufeng@users.noreply.github.com>
2025-07-18 17:43:34 +0800

    Delete genome-assemble directory

--- genome-assemble/Telomere_Centromere.sh
+++
@@ -1,2 +0,0 @@
-perl search_motif.pl -i hap.fa -m motif.txt -o duanli.bed
-python3 quartet.py TeloExplorer -i hap.fa -c plant -o TQS_tm
--- genome-assemble/gap_fill.sh
+++
@@ -1,1 +0,0 @@
-python quartet.py  GapFiller  -d  hap.fa  -g HiFi.ccs.fasta -t 30 --minimapoption \'-x map-hifi\'
